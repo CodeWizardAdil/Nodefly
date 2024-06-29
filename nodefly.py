db49b950bf5f5397cdbcb37888490488f236ce55
adil <adilsaleemkurvattoor@gmail.com>
2024-06-29 04:42:19 +0000

    third

--- nodefly.py
+++ nodefly.py
@@ -1,11 +1,12 @@
 import os
-import paramiko
+
 print(""" _________ 
 / \ 
 | NodeFly | 
 \ / 
 _______/ 
--- by Adil (https://x.com/adil_emmi)""")
+-- by Adil
+(https://x.com/adil_emmi)""")
 os.system('sudo apt update -y && sudo apt install python3-pip')
 print("Your SSH Deployment key is : ")
 GITHUB_REPO = input("Enter Github Repo: ")
