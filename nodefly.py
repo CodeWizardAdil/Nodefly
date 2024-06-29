c5994411a26378227336ae1906448bbf02ed0d29
adil <adilsaleemkurvattoor@gmail.com>
2024-06-29 04:46:38 +0000

    4th

--- nodefly.py
+++ nodefly.py
@@ -7,8 +7,10 @@ print(""" _________
 _______/ 
 -- by Adil
 (https://x.com/adil_emmi)""")
-os.system('sudo apt update -y && sudo apt install python3-pip')
+os.system('sudo apt update -y && sudo apt upgrade -y && sudo apt install python3-pip -y')
 print("Your SSH Deployment key is : ")
 GITHUB_REPO = input("Enter Github Repo: ")
 
 os.system('cd ~ && mkdir Node-project')
+
+os.system('cd ~/Node-project && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash')
